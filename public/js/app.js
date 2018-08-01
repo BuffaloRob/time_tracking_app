08e09a32c5fe12e90fbc3111c10f2699910a2cf5
BuffaloRob <peckham.rob@gmail.com>
2018-08-01 10:57:55 -0400

    EditableTimerList, EditableTImer, and TImersDashboard initial build complete

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,61 @@
 
+class TimersDashboard extends Component {
+  render() {
+    return (
+      <div className="ui three column centered grid">
+        <div className="column">
+          <EditableTimerList />
+          <ToggleableTimerForm 
+            isOpen={true}
+          />
+        </div>
+      </div>
+    );
+  }
+}
+
+class EditableTimerList extends React.Component {
+  render() {
+    return (
+      <div id='timers'>
+        <EditableTimer
+          title='Learn React'
+          project='Web Domination'
+          elapsed='8984300'
+          runningSince={null}
+          editFormOpen={false}
+        />
+        <EditableTimer
+          title='Learn extreme ironing'
+          project='World Domination'
+          elapsed='3890985'
+          runningSince={null}
+          editFormOpen={true}
+        />
+      </div>
+    );
+  }
+}
+
+class EditableTimer extends React.Component {
+  render() {
+    if (this.props.editFormOpen) {
+      return (
+        <TimerForm
+          title={this.props.title}
+          project={this.props.project}
+        />
+      );
+    } else {
+      return (
+        <Timer 
+          title={this.props.title}
+          project={this.props.project}
+          elapsed={this.props.elapsed}
+          runningSince={this.props.runningSince}
+        />
+      );
+    }
+  }
+}
+
