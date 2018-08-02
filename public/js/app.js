aedd0a87ad8153114a0eec51ae5bd2f59b9b9074
BuffaloRob <peckham.rob@gmail.com>
2018-08-02 11:48:52 -0400

    added handleCreateFromSubmit to TimersDashboard

--- public/js/app.js
+++ public/js/app.js
@@ -19,6 +19,17 @@ class TimersDashboard extends React.Component {
     ],
   };
 
+  handleCreateFormSubmit = (timer) => {
+    this.createTimer(timer);
+  };
+
+  createTimer = (timer) => {
+    const t = helpers.newTimer(timer);
+    this.setState({
+      timers: this.state.timers.concat(t),
+    });
+  };
+
   render() {
     return (
       <div className="ui three column centered grid">
@@ -28,6 +39,7 @@ class TimersDashboard extends React.Component {
           />
           <ToggleableTimerForm 
             isOpen={true}
+            onFormSubmit={this.handleCreateFormSubmit}
           />
         </div>
       </div>
